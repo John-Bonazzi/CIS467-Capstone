63ea48791427c6eb7b6ab125bcb69e736f72358a
Clay Negen <negenc@mail.gvsu.edu>
2019-10-23 14:31:01 -0400

    Connected UI to database

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import AppNavBar from './components/AppNavbar'
+import QuestionList from './components/QuestionList';
+import { Provider } from 'react-redux';
+import store from './store';
+import QuestionModal from './components/QuestionModal';
+import { Container } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar/>
+          <Container>
+            <QuestionModal/>
+            <QuestionList/>
+          </Container>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
