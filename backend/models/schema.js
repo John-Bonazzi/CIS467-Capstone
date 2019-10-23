bce80cc918f6a6abcad6429359f2a377b1cffaf6
GionataB <gionatabonazzi@gmail.com>
2019-10-23 14:04:11 -0400

    Defined a schema
    Defined a first version of a schema for our database.
    The schema could change and expand, but the current schema should be the minimum requirements.

--- backend/models/schema.js
+++ backend/models/schema.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Create a Scehma
+/* Create the schema;
+   Eeach element in the schema is made up of:
+   - tag: a human readable unique ID that will be used by the admin panel
+     to extrapolate the ObjectID of an element.
+   - question: the question passed to the front-end.
+   - code: some kind of logic that has to be run to decide the next question.
+     the code element is optional, as only complex questions would need it.
+   - answer: a map of answers, answers can be one or more, and should have
+     and objectID connected to them, or a keyword (TBD) to tell the program
+     to evaluate the code stored in the item. In this case, the key is the answer.
+     If that does not work, in the future it could be changed to a nested element (TBD).
+*/
 const QuestionSchema = new Schema({
-  name: {
+  tag: {
     type: String,
-    required: true,
+    required: true
   },
-  date: {
-    type: Date,
-    default: Date.now,
+  question: {
+    type: String,
+    required: true
   },
+  code: {
+    type: String
+  },
+  answer: {
+    type: Map,
+    of: String, //For now, in the end it could be ObjectIDs
+    required: true
+  }
 });
 
 export let Question = mongoose.model('schema', QuestionSchema);
