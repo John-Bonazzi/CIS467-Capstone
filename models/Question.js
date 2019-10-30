13971a2dba70f1c042b5d4795205a504db29c1f1
Eisenbar <ryan.eisenbarth@gmail.com>
2019-10-30 10:47:04 -0400

    update model and begin question enforcing

--- models/Question.js
+++ models/Question.js
@@ -2,15 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create a Scehma
-const QuestionSChema = new Schema({
-    name: {
-        type: String,
-        required: true
+const QuestionSchema = new Schema({
+    tag: {
+      type: String,
+      required: true,
+      unique: true, //Seems like this does not work properly with mongoose without dropDups
+      dropDups: true //Fails any query that tries to store an element with tag value already existing in the database
     },
-    date: {
-        type: Date,
-        default: Date.now
+    question: {
+      type: String,
+      required: true
+    },
+    code: {
+      type: String
+    },
+    answer: {
+      type: Map,
+      of: String, //For now, in the end it could be ObjectIDs
+      required: true
     }
-});
+  });  
 
-module.exports = Question = mongoose.model('question', QuestionSChema);
+module.exports = Question = mongoose.model('question', QuestionSchema);
