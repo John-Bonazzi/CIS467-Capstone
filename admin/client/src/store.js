7fbd9ec039e3459a432ef450ef59f3753c8f2247
Eisenbar <ryan.eisenbarth@gmail.com>
2019-11-06 08:01:07 -0500

    Restructure files

---
+++ admin/client/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+
+const initialState = {};
+
+const middleware = [thunk];
+
+//Create Redux Store, include __REDUX_DEVTOOLS_EXTENSION__ 
+//for Redux dev tools in browser 
+const store = createStore(
+    rootReducer, 
+    initialState, 
+    compose(
+        applyMiddleware(...middleware)
+    )
+);
+
+export default store;
