e07cf62f5d0ed88e7d50ebe46765d79e390acc6f
GionataB <gionatabonazzi@gmail.com>
2019-11-07 01:03:46 -0500

    Fixed a typo throwing a warning for body-parser

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,8 +15,8 @@ const PORT = process.env.PORT || 5000;
  * and make it readable.
  */
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extendend: true }));
 
 app.use('', router_admin );
 app.use('', router_user);
