bbdd9c2c3a2de78683caa78b1a317fd179a0888a
GionataB <gionatabonazzi@gmail.com>
2019-11-20 15:20:27 -0500

    Commented code

--- backend/src/routing/admin/question_admin.js
+++ backend/src/routing/admin/question_admin.js
@@ -7,31 +7,32 @@ const {questionRoute} = require('./routes');
 const queries = require('./queries/question_queries');
 const routines = require('../../routines/http_routines');
 
-/*
+/**
  * The POST request creates an entry in the database.
- * Notice that the POST will create, not update, an
+ * The POST will create, not update, an
  * entry in the database.
  * PUT should be used to update a database entry.
+ * @name postRoute
+ * @param {Object} req the request received from a client
+ * @param {Object} res the response from the server
+ * @see putRoute
  */
 router.route(questionRoute).post(function(req, res) {
   const entry = new db_admin_entry(req.body);
   queries.postOneElement(entry, function(err){
-    if (err) res.status(500).send('Unable to save to database.');
+    if (err) error_handler.badServerHandler(res, 'Unable to save to database.');
     else res.json('Entry added successfully.');
   });
 });
 
-/*
- * The GET request asks the database for the entry with
- * matching tag.
- *
- * TODO: expand the get with 3 separate functions:
- * 0: get all the database
- * 1: get the matching tag
- * 2: get all the elements with an answer equal to given _id
- * the function will be part of the request body, as a number.
- * function 2 returns all elements that are connected to the given
- * element, searched by _id, as that is what is saved in the answers.
+/**
+ * The GET request offers different options, passed in the request as an 'option' field, which is a string. 'options'default to '1'.
+ * 0: get all the database. This does not require the 'name' body in the request, although its presence does not cause an error.
+ * 1: asks the database for the entry with matching tag. Only one entry is returned.
+ * If something weird happens in the option selection, likely because of an unsupported option, a 400 error is sent back as response.
+ * @name getRoute
+ * @param {Object} req the request received from a client
+ * @param {Object} res the response from the server
  */
 router.route(questionRoute).get(function(req, res) {
   var id = req.body.name;
@@ -49,18 +50,21 @@ router.route(questionRoute).get(function(req, res) {
       break;
 
     default:
-      error_handler.badClientRequest(res);
+      error_handler.badClientRequest(res, "Could not understand 'option' value");
       break;
   }
 });
 
 
-/*
+/**
  * The PUT request is used to modify one entry in the database.
  * The entry has to exist in the database, it does not create one.
  * The validators in the schema will be checked on update: for example,
  * an element's tag cannot be updated if the new tag is already existing in the
  * database.
+ * @name putRoute
+ * @param {Object} req the request received from a client
+ * @param {Object} res the response from the server
  */
 router.route(questionRoute).put(function(req, res) {
   var id = req.body.name;
@@ -69,16 +73,16 @@ router.route(questionRoute).put(function(req, res) {
   queries.updateOneElement(db_admin_entry, {tag: id}, update_param, res);
 });
 
-/*
- * The DELETE request is used to remove an entry from the database.
- *
- * TODO: after an element has been deleted, a search should be made,
- * and all references to the now erased _id should be replaced by a
- * "null" value.
- * Expand it with 3 functions:
- * 0: delete an element
- * 1: delete part of an element
- * 2: delete part of an element in bulk on multiple elements
+/**
+ * The DELETE request is used to remove entries in the database.
+ * Different delete functions are provided, and the option to choose it is passed in the 'option' field in the request body, defaulted at '1'.
+ * The option is a string, and the supported options are the following:
+ * 0: delete all elements. Does not require a 'name' field in the request.
+ * 1: delete a single element
+ * If something weird happens in the option selection, likely because of an unsupported option, a 400 error is sent back as response.
+ * @name deleteRoute
+ * @param {Object} req the request received from a client
+ * @param {Object} res the response from the server
  */
 router.route(questionRoute).delete(function(req, res) {
   var id = req.body.name;
@@ -96,7 +100,7 @@ router.route(questionRoute).delete(function(req, res) {
       break;
     
     default:
-      error_handler.badClientRequest(res);
+      error_handler.badClientRequest(res, "Could not understand 'option' value");
       break;
   }
 });
